4e6cb39409f793095d7b8c5acf600346afc1d9b4
viertor <lukasz.mosinski@gmail.com>
2023-09-29 14:30:31 +0200

    Json api end point

--- FootballClubsManager/Controllers/FootballClubsController.cs
+++ FootballClubsManager/Controllers/FootballClubsController.cs
@@ -28,9 +28,9 @@ namespace FootballClubsManager.Controllers
         }
 
         // GET: FootballClubControllercs/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details()
         {
-            return View();
+            return Ok(_footballClubService.GetClubs());
         }
 
         // GET: FootballClubControllercs/Create
