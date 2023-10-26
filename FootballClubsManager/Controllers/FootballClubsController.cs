28b94fe1191d3f75ccc34e7dc6181b692f3babb4
kaczmarekmarta <146822356+kaczmarekmarta@users.noreply.github.com>
2023-10-26 12:42:47 +0200

    Update FootballClubsController.cs

--- FootballClubsManager/Controllers/FootballClubsController.cs
+++ FootballClubsManager/Controllers/FootballClubsController.cs
@@ -30,7 +30,7 @@ namespace FootballClubsManager.Controllers
         // GET: FootballClubControllercs/Details/5
         public ActionResult Details()
         {
-            return Ok(_footballClubService.GetClubs);
+            return Ok(_footballClubService.GetClubs());
         }
 
         // GET: FootballClubControllercs/Create
